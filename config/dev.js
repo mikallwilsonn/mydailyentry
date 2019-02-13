f5fe234ca5a6a71c47f247eea61f9d21ad99e543
Michael Wilson <info@michaelwilsion.io>
2019-02-12 23:28:24 -0500

    Updated README and made config keys private

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '830061322778-t1am8rdqujg3opgrs27srm338sigaa7e.apps.googleusercontent.com',
-  googleClientSecret: 'Mbt70Bs9ttJf9U45gJk0yZY8',
-  mongoURI: 'mongodb://admin:1wNcJjl5rcl2PHLd@cluster0-shard-00-00-ncanf.mongodb.net:27017,cluster0-shard-00-01-ncanf.mongodb.net:27017,cluster0-shard-00-02-ncanf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
